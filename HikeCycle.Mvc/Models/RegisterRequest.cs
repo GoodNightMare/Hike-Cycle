3398b7f59337ccf644e2fd7f29f029c0c5db871c
GoodNightMare <nonthee.night@gmail.com>
2026-03-24 14:38:28 +0700

    c#
    model
    
    admindashboard

---
+++ HikeCycle.Mvc/Models/RegisterRequest.cs
@@ -0,0 +1,8 @@
+    public class RegisterRequest
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public string? FullName { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+    }
