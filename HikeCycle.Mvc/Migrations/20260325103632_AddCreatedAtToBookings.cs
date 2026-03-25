6a616fa976ec5bdb8467c2a3ccf4e726971a7109
GoodNightMare <nonthee.night@gmail.com>
2026-03-25 17:54:55 +0700

    migration

---
+++ HikeCycle.Mvc/Migrations/20260325103632_AddCreatedAtToBookings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HikeCycle.Mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedAtToBookings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
