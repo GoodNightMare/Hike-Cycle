dbe36ff456bd2e89b4dd11f2671a81ddf7ed0f49
GoodNightMare <nonthee.night@gmail.com>
2026-03-25 17:56:31 +0700

    add success page / show info of item in cart

--- HikeCycle.Mvc/Models/PaymentViewModel.cs
+++ HikeCycle.Mvc/Models/PaymentViewModel.cs
@@ -5,5 +5,7 @@ namespace HikeCycle.Mvc.Models
         public int BookingId {get; set;}
         public decimal Amount { get; set; }
         public string Method { get; set; }
+        public decimal OriginalTotal { get; set; }
+        public decimal TotalDiscount { get; set; }
     }
 }
